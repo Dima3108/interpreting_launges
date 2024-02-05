6009d79a767e7e6c30fba59ff127ce1da693a585
Dima <73338242+Dima3108@users.noreply.github.com>
2024-02-05 19:27:25 +0300

    Number1
    1 Задание

--- Lab1.py
+++ Lab1.py
@@ -1,2 +1,21 @@
 N = 12
 print(((N-1)%12)-1)
+#Вариан 10
+#Номер 1
+def isProst(x,y):
+    if x%y==0:
+        return  False
+    else:
+        return  True
+def getCount(x):
+    count=0
+    y=range(2,x,2)
+    for i in y:
+        if isProst(x,i)==False:
+            count+=1
+    return  count
+val=int(input())
+print(getCount(val))
+
+
+
