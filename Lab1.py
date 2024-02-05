54b5f3746fee34bd5c2f7e3b39ee5b91fb1ad6d0
Dima <73338242+Dima3108@users.noreply.github.com>
2024-02-05 19:55:06 +0300

    Number 3
    Задание 3

--- Lab1.py
+++ Lab1.py
@@ -33,5 +33,35 @@ if res != -1 :
     print(res)
 else:
     print("подходящие цифры отсутсвуют")
+#Задание 3
+def ND(x): #Наименьший делитель не равный 1
+    min = x
+    val = x
+    while val > 1 :
+        if( x % val == 0):
+            min = val
+        val=val-1
+    return min
+def ChifSum(x): #Сумма цифр < 5
+    sum =0
+    tmp=x
+    while tmp > 0:
+        if (tmp % 10) < 5 :
+            sum = sum + (tmp % 10)
+        tmp=int(tmp/10)
+    return sum
+def Proizv(x):
+    ran1=range(1,x)
+    max = -1
+    nd= ND(x)
 
-3
+    for i in ran1:
+        if (i % nd !=0) and (x % i !=0):
+            max=i
+    return max * sum(x)
+val=int(input())
+r=Proizv(val)
+if r <0 :
+    print("отсутсвует подходящие число")
+else :
+    print(r)
