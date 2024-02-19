5b1d15173dcc0e247934aadfa910b01ba955e0cf
Dima <73338242+Dima3108@users.noreply.github.com>
2024-02-19 16:40:51 +0300

    add task15 for Lab3

--- vs/PythonApplications/Lab1_task3.py
+++ vs/PythonApplications/Lab1_task3.py
@@ -7,4 +7,11 @@ def task_4(str):
          i=i+1
     return  count
 
-
+def task_15(str):
+    count = 0
+    i = 0
+    while (i < len(str)):
+        if (str[i] >= '6' and str[i] <= '9'):
+            count = count + 1
+        i = i + 1
+    return count
