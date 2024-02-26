1ab93de2f0e908c26b1a704ad29d5d1242d8db52
Dima <73338242+Dima3108@users.noreply.github.com>
2024-02-26 11:57:43 +0300

    python311
    Пересборка под python 3.11

--- vs/ForPython/obj/Debug/netstandard2.0/ForPython.AssemblyInfo.cs
+++ vs/ForPython/obj/Debug/netstandard2.0/ForPython.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForPython")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e1f649ed0f07629e8dc5befc77a6530b227af3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b5e87c21ff15e88e94a3f5b26d919259dd0e71")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForPython")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForPython")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
