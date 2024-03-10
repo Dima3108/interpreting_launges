e4d586ae362ec310f9dc9a27dd6efd616619acdc
Dima <73338242+Dima3108@users.noreply.github.com>
2024-03-10 15:03:09 +0300

    task 5
    Доделал задачу 5

--- vs/PythonApplications/Lab1_task4.py
+++ vs/PythonApplications/Lab1_task4.py
@@ -5,7 +5,7 @@ tablesg = "qwrtpsdfghjklzxcvbnmйцкнгшщзхъфвпрлджэчсмтьб"
 #количеством гласных букв в строке.
 def task_1(str):
     strlow=str.low()
-    sarray=strlow.split(' ')
+    sarray=strlow.split('\n')
     delta_a=[]
     for s in sarray :
         countq=0
@@ -58,6 +58,78 @@ def task_5(str):
            char_table.append(str[i])
        i=i+1
    j=0
+   #Подсчет частот
+   glob_freqs=[]
    while(j<len(char_table)):
-        count
+        count=0
+        i=0
+        while(i<len(str)):
+            if(str[i]==char_table[j]):
+                count=count+1
+            i=i+1
         j=j+1
+        glob_freqs.append(count)
+    #разбиение на строки
+   strings_=str.split('\n')
+   def compute_freq_in_string(string_,gl_ch,gl_fr):
+       local_chars=[string_[0]]
+       i=1
+       while(i<len(string_)):
+           s=1
+           j=0
+           while(j<len(local_chars)):
+               if(local_chars[j]==string_[i]):
+                   s=-1
+                   break
+               j=j+1
+           if(s==1):
+               local_chars.append(string_[i])
+           i=i+1
+       local_freqs=[]
+       j=0
+       while(j<len(local_chars)):
+           lcount=0
+           i=0
+           while(i<len(string_)):
+               if(string_[i]==local_chars[j]):
+                   count=count+1
+               i=i+1
+           j=j+1
+           local_freqs.append(count)
+       max_freq=local_freqs[0]
+       max_freq_indecs=0
+       i=0
+       while(i<len(local_freqs)):
+           if(max_freq<local_freqs[i]):
+               max_freq=local_freqs[i]
+               max_freq_indecs=i
+           i=i+1
+       j=0
+       while(gl_ch[j]!=local_chars[max_freq_indecs]):
+           j=j+1
+       return (gl_fr[j]-local_freqs[max_freq_indecs])*(gl_fr[j]-local_freqs[max_freq_indecs])
+   deltas=[]
+   i=0
+   while(i<len(strings_)):
+       deltas.append(compute_freq_in_string(strings_[i]))
+       i=i+1
+   new_str=[]
+   count=0
+   l=len(strings_)
+   while(count<l):
+       max=deltas[0]
+       i=0
+       max_pos=0
+       while(i<len(strings_)):
+           if(max<deltas[i]):
+               max=deltas[i]
+               max_pos=i
+           i=i+1
+       new_str.append(strings_[max_pos])
+       strings_.pop(max_pos)
+       deltas.pop(i)
+       l=l+1
+   return new_str   
+
+   
+   
