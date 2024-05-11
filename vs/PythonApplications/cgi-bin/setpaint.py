5b9cb9a244683f533fb1d921593a67b2ebe041c4
Dima <73338242+Dima3108@users.noreply.github.com>
2024-05-11 19:43:17 +0300

    task 6
    Выполнил 6 задачу

--- vs/PythonApplications/cgi-bin/setpaint.py
+++ vs/PythonApplications/cgi-bin/setpaint.py
@@ -7,8 +7,8 @@ connection = sqlite3.connect('painting_database.db')
 #form = cgi.FieldStorage()  
 #connection = sqlite3.connect('painting_database.db')
 paintname=str(form.getvalue("pname"))
-painttypeid=str(form.getvalue("typeid"))
-paintauthorid=str(form.getvalue("authorid"))
+painttypeid=int(form.getvalue("typeid"))
+paintauthorid=int(form.getvalue("authorid"))
 print("Content-type: text/html;Accept-Charset: utf-8")
 print('''
 <!DOCTYPE html>
@@ -36,16 +36,24 @@ print('''
     <body>
 ''')
 cursor = connection.cursor()
-cursor.execute("select * from iatp where iatp.name='%s' and iatp.authorid = '%s' and iatp.type = '%s'" %(paintname,paintauthorid,painttypeid))
+ch="'"
+names=paintname
+types=painttypeid
+auth_id=paintauthorid
+cursor.execute("select * from iatp where iatp.name= '"+str(names)+"' and iatp.type = '"+str(types)+"' and iatp.authorid = '"+str(auth_id)+"'")
 stat_=-1
 for elem in cursor.fetchall():
     stat_=1
 if(stat_==1):
-    print("указанный тип (%s:%s:%s) существует!" %(paintname,paintauthorid,painttypeid))
+    print("указанный тип ("+paintname+":"+paintauthorid+":"+painttypeid+") существует!" %(paintname,paintauthorid,painttypeid))
 else:
-    cursor.execute("insert into iatp(name,type,authorid) VALUES('%s','%s','%s')" %(paintname,paintauthorid,painttypeid))
-    print("указанный тип (%s:%s:%s) успешно добавлен" %(paintname,paintauthorid,painttypeid))
+    s='''insert into iatp(name,type,authorid) VALUES('''+"'"+str(names)+ch+','+ch+str(types)+ch+','+ch+str(auth_id)+ch+')'
+    print(s)
+    cursor.execute(s)
     connection.commit()
+    #cursor.execute("insert into iatp(name,type,authorid) VALUES("+paintname+","+painttypeid+","+paintauthorid+")" )
+    print("указанный тип ("+str(paintname)+":"+str(paintauthorid)+":"+str(painttypeid)+") успешно добавлен" )
+    #connection.commit()
 print('''
        <a href="index.py">На главную</a>
   </body>
