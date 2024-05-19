0e38548caaded314026ebd59dba7057615653048
Dima <73338242+Dima3108@users.noreply.github.com>
2024-05-19 17:02:34 +0300

    t

--- vs/PythonApplications/Lab7.py
+++ vs/PythonApplications/Lab7.py
@@ -1,2 +1,5 @@
 import django
 #https://django.fun/docs/django/4.0/intro/tutorial01/
+#https://django.fun/docs/django/4.0/intro/tutorial03/
+#https://django.fun/docs/django/4.0/intro/tutorial04/
+#https://django.fun/docs/django/4.0/intro/tutorial05/
--- vs/PythonApplications/task7prog/content/views.py
+++ vs/PythonApplications/task7prog/content/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+#from django.shortcuts import render
+from django.http import Http404
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'content/index.html', context)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'content/detail.html', {'question': question})
 # Create your views here.
