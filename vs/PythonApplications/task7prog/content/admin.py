ce5df1ff064bbbd16a59c5dead64f57d924e9bd6
Dima <73338242+Dima3108@users.noreply.github.com>
2024-05-26 16:13:42 +0300

    y

--- vs/PythonApplications/task7prog/content/admin.py
+++ vs/PythonApplications/task7prog/content/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Question
+#from .models import Question
 from .models import iatp
 from .models import type_of_painting
 from .models import authors
-admin.site.register(Question)
+#admin.site.register(Question)
 admin.site.register(type_of_painting)
 admin.site.register(iatp)
 admin.site.register(authors)
--- vs/PythonApplications/task7prog/content/views.py
+++ vs/PythonApplications/task7prog/content/views.py
@@ -1,16 +1,24 @@
+
 from django.shortcuts import get_object_or_404, render
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.http import Http404
-from .models import Question
+#from .models import Question
 from django.http import HttpResponse
 from django.template import loader
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'content/index.html', context)
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'content/detail.html', {'question': question})
+from pindex import *
+def index(requets):
+    c=get_content()
+    html = "<html><body>It is now %s.</body></html>" % c
+    return HttpResponse(html)
+    #return redirect("Content/Pages/Index.py")
+#def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    context = {'latest_question_list': latest_question_list}
+#    return render(request, 'content/index.html', context)
+#def detail(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'content/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
